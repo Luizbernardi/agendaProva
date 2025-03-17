4e1bb716b3ba44376dbb601cbe3b835907f6091e
LuizBernardi <luizbernardi37@gmail.com>
2025-03-17 20:17:27 -0300

    adicionando categoria

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Contato
+from .models import Contato, Categoria
 
 
 @admin.register(Contato)
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ("nome", "telefone", "email", "endereco")
     search_fields = ("nome", "telefone", "email")
+    list_filter = ("nome",)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
     list_filter = ("nome",)
--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200, verbose_name="Nome")
+
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=200, verbose_name="Nome")
     telefone = models.CharField(max_length=15, verbose_name="Telefone")
