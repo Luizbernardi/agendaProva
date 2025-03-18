5fdb8229ec13f64d441b49101b643140bd0ad75b
LuizBernardi <luizbernardi37@gmail.com>
2025-03-17 22:26:46 -0300

    adicionando cadastro listagem e detalhes categoria

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ContatoListView, ContatoDetalhesView, ContatoCreateView, HomeView
+from .views import ContatoListView, ContatoDetalhesView, ContatoCreateView, HomeView, CategoriaListView, CategoriaDetalhesView, CategoriaCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('listagem/', ContatoListView.as_view(), name='listagem_contatos'),
-    path('detalhes/<int:pk>/', ContatoDetalhesView.as_view(), name='contato_detalhe'),
-    path('cadastro/', ContatoCreateView.as_view(), name='contato_cadastro')
+    path('contato/listagem', ContatoListView.as_view(), name='listagem_contatos'),
+    path('contato/detalhes/<int:pk>/', ContatoDetalhesView.as_view(), name='contato_detalhe'),
+    path('contato/cadastro/', ContatoCreateView.as_view(), name='contato_cadastro'),
+    path('categoria/listagem', CategoriaListView.as_view(), name='listagem_categorias'),
+    path('categoria/detalhes/<int:pk>/', CategoriaDetalhesView.as_view(), name='categoria_detalhe'),
+    path('categoria/cadastro/', CategoriaCreateView.as_view(), name='categoria_cadastro'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -18,10 +18,21 @@ class ContatoListView(ListView):
     context_object_name = 'contatos'
     paginate_by = 15
 
+class CategoriaListView(ListView):
+    model = Categoria
+    template_name = 'myapp/listagem_categorias.html'
+    context_object_name = 'categorias'
+    paginate_by = 15    
+
 class ContatoDetalhesView(DetailView):
     model = Contato
     template_name = 'myapp/contato_detalhe.html'
     context_object_name = 'contato'
+
+class CategoriaDetalhesView(DetailView):
+    model = Categoria
+    template_name = 'myapp/categoria_detalhe.html'
+    context_object_name = 'categoria'
 
 class ContatoCreateView(CreateView):
     model = Contato
@@ -34,6 +45,18 @@ class ContatoCreateView(CreateView):
         context['categorias'] = Categoria.objects.all() 
         return context
 
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        return response
+    
+
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    template_name = 'myapp/categoria_cadastro.html'
+    fields = ['nome']
+    success_url = reverse_lazy('listagem_categorias')
+
+    
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         return response
