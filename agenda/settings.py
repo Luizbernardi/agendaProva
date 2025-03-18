29770745c96151b0d1546527596626af6d34b87b
LuizBernardi <luizbernardi37@gmail.com>
2025-03-17 23:04:30 -0300

    mudando style home

--- agenda/settings.py
+++ agenda/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL base para arquivos estáticos
+
+
+# Diretório onde os arquivos estáticos serão coletados em produção
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
